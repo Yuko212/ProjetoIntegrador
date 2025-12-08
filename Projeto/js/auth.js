dcfc435390d9220a2844d9480b27d672b964982b
mlsd24 <malu240296@gmail.com>
2025-12-08 10:35:24 -0300

    commit

--- Projeto/js/auth.js
+++ Projeto/js/auth.js
@@ -1,20 +1,20 @@
-// Verifica se o usuário está logado
+
 function verificarLogin() {
     const usuario = localStorage.getItem("usuarioLogado");
 
     if (!usuario) {
-        // não está logado → manda para o login
-        window.location.href = "login.html";
+       
+        alert("Não logado");
     }
 }
 
-// Retorna o objeto do usuário logado
+
 function getUsuario() {
     const usuario = localStorage.getItem("usuarioLogado");
     return usuario ? JSON.parse(usuario) : null;
 }
 
-// Faz logout
+
 function logout() {
     localStorage.removeItem("usuarioLogado");
     window.location.href = "login.html";
